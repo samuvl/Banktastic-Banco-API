a31c2233fd17f09b26ef20219515b3439f31df3d
Lliurex <Lliurex@172.16.205.116>
2016-01-15 13:49:18 +0100

    Falta GET y UPDATE cuenta (usuario, que lo enseñe)

--- web/cuentabancaria/FindCuentaController.js
+++ web/cuentabancaria/FindCuentaController.js
@@ -3,12 +3,6 @@ FindCuentaController.$inject = ['$scope', '$routeParams', 'cuentaBancariaService
 function FindCuentaController($scope, $routeParams, cuentaBancariaService, usuarioService) {
     $scope.tipo = "FIND";
 
-    usuarioService.find().then(function (result) {
-        $scope.usuarios = result.data;
-    }, function (result) {
-        alert("Ha fallado la petición. Estado HTTP:" + result.status);
-    });
-
     cuentaBancariaService.find().then(function (result) {
         $scope.cuentasBancarias = result.data;
     }, function (result) {
