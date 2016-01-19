0b91b98346157faaf5806280017edf2c9ad53db3
Lliurex <Lliurex@172.16.205.116>
2016-01-19 14:01:52 +0100

    Input búsqueda DNI cuenta. Entidad maestro sucursal detalle. Falta validaciones en general.

--- web/movimiento/InsertMovimientoController.js
+++ web/movimiento/InsertMovimientoController.js
@@ -1,23 +1,30 @@
-InsertMovimientoController.$inject = ['$scope', 'movimientoService', '$location', 'cuentaBancariaService','sharedPropierties'];
+InsertMovimientoController.$inject = ['$scope', 'movimientoService', '$location', 'cuentaBancariaService', 'sharedPropierties'];
 
-function InsertMovimientoController($scope, movimientoService, $location, cuentaBancariaService, sharedPropierties){
-    
-    
+function InsertMovimientoController($scope, movimientoService, $location, cuentaBancariaService, sharedPropierties) {
+
+
     $scope.movimiento = {};//tipomovimiento, concepto, importe,
-    
+
     cuentaBancariaService.get(sharedPropierties.getProperty()).then(function (result) {
         $scope.movimiento.cuentaBancaria = result.data;
-        $scope.movimiento.saldo = $scope.movimiento.cuentaBancaria.saldo;
-    }, function (result){        
-        alert("Ha fallado la peticion. Estado HTTP:"+result.status);
-    }); 
-    
-     $scope.ok = function () {       
-        
-         $scope.movimiento.fechaMovimiento = new Date(); //añado la fecha para que sea la de hoy
-         
+    }, function (result) {
+        alert("Ha fallado la peticion. Estado HTTP:" + result.status);
+    });
+
+    $scope.ok = function () {
+
+        $scope.movimiento.fechaMovimiento = new Date(); //añado la fecha para que sea la de hoy
+        $scope.movimiento.saldoAnterior = $scope.movimiento.cuentaBancaria.saldo;
+
+        if ($scope.movimiento.tipoMovimiento === 'debe') {
+            $scope.movimiento.saldoPosterior = $scope.movimiento.cuentaBancaria.saldo - $scope.movimiento.importe;
+        } else {
+            $scope.movimiento.saldoPosterior = parseInt($scope.movimiento.cuentaBancaria.saldo) + parseInt($scope.movimiento.importe);
+        }
+
         movimientoService.insertMovimiento($scope.movimiento).then(function (result) {
             alert("Movimiento Insertado con Éxito");
+            $location.url('/updateCuenta/' + sharedPropierties.getProperty());
         }, function (result) {
             if (result.status === 500) {
                 alert("Ha fallado la petición. Estado HTTP:" + result.status);
