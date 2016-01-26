b7621e4f20f91fdc22fdd3126dae71964b2f8ef7
ralliv <villarmoto@gmail.com>
2016-01-26 12:12:12 +0100

    commit villar:
    - Implementado el TransactionController (y funcional)
    - En movimientoBancarioController elimnados algunos comentarios

---
+++ src/java/com/fpmislata/banco/presentacion/controllers/TransaccionController.java
@@ -0,0 +1,64 @@
+
+
+package com.fpmislata.banco.presentacion.controllers;
+
+import com.fpmislata.banco.business.domain.Transaccion;
+import com.fpmislata.banco.business.service.TransaccionService;
+import com.fpmislata.banco.core.BusinessException;
+import com.fpmislata.banco.core.BusinessMessage;
+import com.fpmislata.banco.presentacion.json.JsonTransformer;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TransaccionController {
+    
+    @Autowired
+    TransaccionService transaccionService;
+    
+    @Autowired
+    JsonTransformer jsonTransformer;
+    
+    @RequestMapping(value = {"/transaccion"}, method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public void insertTransaccion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, @RequestBody String jsonEntrada) {
+        try {
+            
+            Transaccion transaccion = (Transaccion) jsonTransformer.jsonToObject(jsonEntrada, Transaccion.class);
+            transaccionService.insertTransaccion(transaccion);
+            
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            httpServletResponse.setContentType("application/json; charset=UTF-8");
+            httpServletResponse.getWriter().println("La transaccion se ha realizado correctamente");
+
+        } catch (BusinessException ex) {
+            List<BusinessMessage> bussinessMessage = ex.getBusinessMessages();
+            String jsonSalida = jsonTransformer.objectToJson(bussinessMessage);
+
+            httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            httpServletResponse.setContentType("application/json; charset=UTF-8");
+            try {
+                httpServletResponse.getWriter().println(jsonSalida);
+            } catch (IOException ex1) {
+                Logger.getLogger(EntidadBancariaController.class.getName()).log(Level.SEVERE, "Error devolviendo Lista de Mensajes", ex1);
+            }
+        } catch (Exception ex1) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            httpServletResponse.setContentType("text/plain; charset=UTF-8");
+            try {
+                ex1.printStackTrace(httpServletResponse.getWriter());
+            } catch (IOException ex2) {
+                Logger.getLogger(EntidadBancariaController.class.getName()).log(Level.SEVERE, "Error devolviendo la traza", ex2);
+            }
+        }
+    }
+
+}
